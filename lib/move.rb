8b541f3698e3f86c5df0fd8827ec67118d5f2031
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-18 19:11:30 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,7 +9,7 @@ end
 # code your input_to_index and move method here!
 
 def input_to_index(input)
-  index = input.to_i - 1
+  return index = input.to_i - 1
 end
 
 def move(arr, index, character = "X")
